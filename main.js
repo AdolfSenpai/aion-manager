aeed5b4e215eecd69f6348735b60da7ff85ca7af
ed <edsim97@gmail.com>
2023-09-15 07:43:19 +0300

    initialize firebase

---
+++ main.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB6gLCAd3vsb-QYSAFhh_6BjjnruAxlJjI",
+  authDomain: "aion-2023.firebaseapp.com",
+  projectId: "aion-2023",
+  storageBucket: "aion-2023.appspot.com",
+  messagingSenderId: "776192316285",
+  appId: "1:776192316285:web:f0c60e974ee605ad0a3214"
+};
+
+const app = initializeApp(firebaseConfig);
