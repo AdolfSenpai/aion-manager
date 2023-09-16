4037655e21c2dad5579fa5047c7325f9a6083350
ed <edsim97@gmail.com>
2023-09-16 20:46:22 +0300

    types setup

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,6 @@
-// Import the functions you need from the SDKs you need
+import { getAuth } from "@firebase/auth";
 import { initializeApp } from "firebase/app";
-import _ from "lodash";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyB6gLCAd3vsb-QYSAFhh_6BjjnruAxlJjI",
   authDomain: "aion-2023.firebaseapp.com",
@@ -15,9 +10,5 @@ const firebaseConfig = {
   appId: "1:776192316285:web:f0c60e974ee605ad0a3214"
 };
 
-// Initialize Firebase
-export const firebaseApp = initializeApp(firebaseConfig);
-
-$(function() {
-  $("body").append($("<div>Hello</div>"))
-});
+const firebaseApp = initializeApp(firebaseConfig);
+const auth = getAuth(firebaseApp);
